20193a09539eac142efc2db888586c9f18c16d98
Lethicia Asevedo <lethiciaasevedo01@gmail.com>
2021-03-06 20:48:29 -0300

    Entendendo arrays

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
-var nome = "Lethicia Asevedo";
-var idade = 22;
-alert(nome + "tem" + idade + "anos");
+
+var frutas = [{nome: "maça", cor:"vermelha"}, {nome: "uva", cor:"roxa"}];
+console.log(frutas);
+alert(frutas[1].nome);
+
+
+
+
+
+//*var nome = "Lethicia Asevedo";
+//var idade = 22;
+//alert(nome + "tem" + idade + "anos"); 
+
+//var lista = ["maça", "pêra", "laranja"];
+//lista.push("uva")
+
+
+//console.log(lista.reverse());
+//console.log(lista.toString());
+//console.log(lista.join("-"));
+
+
+//alert(lista[1]);
