9dc2dde76c8a55995ea2b7f3ab922506efc57c94
Lethicia Asevedo <lethiciaasevedo01@gmail.com>
2021-03-06 21:10:35 -0300

    Condicionais, laços de repetiçao e Date

--- js/main.js
+++ js/main.js
@@ -1,9 +1,40 @@
 
-var frutas = [{nome: "maça", cor:"vermelha"}, {nome: "uva", cor:"roxa"}];
-console.log(frutas);
-alert(frutas[1].nome);
+
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+
+// mesma estrutura feita anteriormente, porém com o "for"
+/*var count;
+for(count=0; count <= 5; count++){
+    alert(count);
+}
 
 
+/*var count = 0; 
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count++;
+
+}*/
+
+
+/*var idade = prompt ("Qual a sua idade ?")
+
+if(idade >= 18){
+    alert("Maior de idade");
+}else{
+    alert("Menor de idade");
+};*/ 
+
+
+
+
+//var frutas = [{nome: "maça", cor:"vermelha"}, {nome: "uva", cor:"roxa"}];
+//console.log(frutas);
+//alert(frutas[1].nome);
 
 
 
